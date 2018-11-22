782d078f1703b18d0066feb221a2f25bc57fc765
Serje <shepelevich.serje@gmail.com>
2018-11-22 18:27:17 +0300

    fully configured data flow for generate tests

--- TestGenerator.Runner/TestGenerator/Generator.cs
+++ TestGenerator.Runner/TestGenerator/Generator.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
@@ -24,13 +22,31 @@ namespace TestGenerator
 
         public Task Generate(List<string> paths)
         {
+            var codeParser = new CodeParser();
+
             var reader = new TransformBlock<string, Task<string>>(inputData => ReadFileAsync(inputData),
                 new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = readingThreadAmout });
+
+            var generator = new TransformBlock<Task<string>, Task<List<GeneratedTestResult>>>(
+                inputData => codeParser.GenerateTestsAsync(inputData, outputDirectory),
+                new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxProcessingThreadAmout });
 
             var writer = new ActionBlock<Task<List<GeneratedTestResult>>>((generatedClass => WriteFileAsync(generatedClass)),
                 new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = writingThreadAmout });
 
-            return null;
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+            reader.LinkTo(generator, linkOptions);
+            generator.LinkTo(writer, linkOptions);
+
+            foreach (var path in paths)
+            {
+                reader.Post(path);
+            }
+                
+            reader.Complete();
+
+            return writer.Completion;
         }
 
         private async Task<string> ReadFileAsync(string path)
