7ae746ab8d0c26fc2b8cb259fe51d5a1257e3ee5
Serje <shepelevich.serje@gmail.com>
2018-11-22 17:54:09 +0300

    added reader-transformer form string to Task<string>

--- TestGenerator.Runner/TestGenerator/Generator.cs
+++ TestGenerator.Runner/TestGenerator/Generator.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace TestGenerator
 {
-    class Generator
+    public class Generator
     {
+        string outputDirectory;
+        int readingThreadAmout;
+        int writingThreadAmout;
+        int maxProcessingThreadAmout;
+
+        Generator(string outputDirectory, int readingThreadAmout, int writingThreadAmout, int maxProcessingThreadAmout)
+        {
+            this.outputDirectory = outputDirectory;
+            this.readingThreadAmout = readingThreadAmout;
+            this.writingThreadAmout = writingThreadAmout;
+            this.maxProcessingThreadAmout = maxProcessingThreadAmout;
+        }
+
+        public Task Generate(List<string> paths)
+        {
+            var reader = new TransformBlock<string, Task<string>>(inputData => ReadFileAsync(inputData),
+                new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = readingThreadAmout });
+            return null;
+        }
+
+        private async Task<string> ReadFileAsync(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                return await sr.ReadToEndAsync();
+            }
+        }
+
     }
 }
